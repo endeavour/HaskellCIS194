3a369b36640edb845be68a9bb4a8397706f89185
James Freiwirth <james.freiwirth@asos.com>
2018-04-30 15:45:50 +0100

    Use NonEmpty list to avoid 'dangerous' non-safe head call

--- src/Homework3.hs
+++ src/Homework3.hs
@@ -1,9 +1,10 @@
 module Homework3 where
 
-import Data.List (take, drop, map, group, sort)
+import Data.List (take, drop, map, sort)
 import Data.List.Index (imap)
 import Data.HashMap.Strict (fromList, lookupDefault)
 import Data.List.Split (chunksOf)
+import Data.List.NonEmpty (group, head)
 
 every :: Int -> [a] -> [a]
 every n [] = []
@@ -28,7 +29,7 @@ localMaxima xs =
 
 histogram :: [Integer] -> String
 histogram xs =
-  let frequencies        = map (\x -> (head x, length x)) $ group $ sort xs
+  let frequencies        = map (\x -> (Data.List.NonEmpty.head x, length x)) $ group $ sort xs
       graphHeight        = maximum $ map (\(_, freq) -> freq) frequencies
       freqMap            = fromList frequencies
       graphChar n height = if lookupDefault 0 n freqMap >= height then '*' else ' '
